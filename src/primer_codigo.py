e0b0f515498eabd49f30336714f23948ac26c0b6
Ferney Bohorquez <ferneyb@gmail.com>
2022-09-24 08:47:06 -0500

    primer commit

---
+++ src/primer_codigo.py
@@ -0,0 +1,36 @@
+from traceback import FrameSummary
+
+
+#print("Hola Mundo") #imprime una cadena de caracteres
+# Variables numericas
+#x = 5.0
+#y = 3
+#frase='frase'
+#print('X es igual a ', x)
+#print('Y es igual a:', y)
+#type(y)
+#type(x)
+# Existen as variables enters (int), flotantes (float), y string(str)
+#print(x, 'es una variable tipo', type(x))
+#print(y, 'es una variable tipo', type(y))
+#print(frase, 'es una variable tipo', type(frase))
+# Operaciones entre variables numericas
+#print('Suman:',x+y)
+#print('Resta:',x-y)
+#print('multiplicacion:',x*y)
+#print('Division:',x/y)
+#print('Potencia:',x**y)
+#print('Modulo:',x%y)
+# LOOPS (Iteraciones)
+#for num in range(1, 11):    # moviendose sobre una lista de numeors
+#    if num % 3 == 0:        # es divisible por 3?
+#        print(num)          # imprimir el numero
+#        break
+#    else:
+#        print(num,'No es divisible por 3')
+# Operaciones con strings
+frase = "Hola mundo"
+frase2 = "Big Data"
+frase + frase2  # Concatenar strings
+n_repeticiones = 10
+n_repeticiones*10  
